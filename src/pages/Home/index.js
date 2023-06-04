1c363051847ca049981cd42ba2be89aca9f93f55
Marco Junior <marco_r_c_junior@hotmail.com>
2023-06-04 15:07:31 -0300

    alteracoes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -20,24 +20,44 @@ const Home = () => {
           </Typography>
           <br />
 
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image="https://solutudo-cdn.s3-sa-east-1.amazonaws.com/prod/adv_files/5d2f2382-c82c-4537-b1c6-0cacac1e0eb7/a264db2f-590d-4ce5-9028-0c4266d5c85b.jpg"
-              title="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Clientes
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lista declientes da Estética Automotiva
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Ver lista</Button>
-            </CardActions>
-          </Card>
+          <Container row style={{ width: '100%', justifyContent: 'space-around' }}>
+            <Card className='margin-not-left' sx={{ flex: 1 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://solutudo-cdn.s3-sa-east-1.amazonaws.com/prod/adv_files/5d2f2382-c82c-4537-b1c6-0cacac1e0eb7/a264db2f-590d-4ce5-9028-0c4266d5c85b.jpg"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Clientes
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lista declientes da Estética Automotiva
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" onClick={() => navigate('/clientes')}>Ver lista</Button>
+              </CardActions>
+            </Card>
+
+            <Card className='margin-not-left' sx={{ flex: 1 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://jetfilm.com.br/blog/wp-content/uploads/2021/03/detail-e-estetica-automotiva-sao-paulo.jpg"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Serviços
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lista de serviços disponiveis na Estética Automotiva
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" onClick={() => navigate('/clientes')}>Ver lista</Button>
+              </CardActions>
+            </Card>
+
+          </Container>
 
         </Container>
       </Container>
